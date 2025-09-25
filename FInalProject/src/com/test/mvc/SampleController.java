38d5c3982027e08955361d541f3ecbab728ed7a4
kjin5424 <kjin5424@gmail.com>
2025-09-25 12:07:28 +0900

    2500925_원본

---
+++ FInalProject/src/com/test/mvc/SampleController.java
@@ -0,0 +1,26 @@
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// - 사용자 정의 컨트롤러
+
+// ※ Spring MVC의 『Controller』 인터페이스를 구현하는 방법을 통해
+//    사용자 정의 컨트롤러 클래스를 구성할 수 있도록 한다.
+
+public class SampleController implements Controller
+{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		// Controller가 수행해야 하는 액션 코드
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+	
+}
