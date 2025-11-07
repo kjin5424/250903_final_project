6084f562ea5e675284e3d8c328ee21386d0c9818
Newbiedevelope <104681437+Newbiedevelope@users.noreply.github.com>
2025-11-07 18:01:19 +0900

    좋다~

--- FinalProject/250903_finalproject/src/com/test/mybatis/dao/IUserDAO.java
+++ FinalProject/250903_finalproject/src/com/test/mybatis/dao/IUserDAO.java
@@ -25,4 +25,9 @@ public interface IUserDAO
 	// 모임 컨텐츠 생산 폼 요청 시 해당 유저가 해당 모임의 모임원인지 검증하는 메소드
 	public String checkMember(@Param("userCode") String userCode, @Param("groupApplyCode") String groupApplyCode);
 	
+	// 마이페이지 열람시 필요한 항목(닉네임, 이메일, 사진, 현재 운영/가입중인 모임 등)
+	public UserDTO inMyPage(@Param("userCode")String userCode);
+	
+
+	
 }
