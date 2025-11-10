cf1f02840688bbc94f3c15cd394f7cc734211aff
sist166 <sist166@sist>
2025-11-10 11:15:56 +0900

    challenge miwansung

---
+++ FinalProject/250903_finalproject/src/com/test/mybatis/controller/group_room/challenge/ChanllengeController.java
@@ -0,0 +1,195 @@
+package com.test.mybatis.controller.group_room.challenge;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.test.mybatis.dao.IChallengeDAO;
+import com.test.mybatis.dto.ChallengeContentDTO;
+import com.test.mybatis.dto.ChallengeDTO;
+import com.test.mybatis.dto.UserDTO;
+
+@Controller
+public class ChanllengeController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	
+	// 도전과제 리스트 출력
+	@RequestMapping(value="/challengelist.do")
+	public String challengeList(Model model, HttpSession session, HttpServletRequest request)
+	{
+		IChallengeDAO dao = sqlSession.getMapper(IChallengeDAO.class);
+		UserDTO user = (UserDTO)session.getAttribute("user");
+		String groupApplyCode = request.getParameter("GroupApplyCode");
+		
+		
+		// 유저의 joinCode 기반으로 GroupApplyCode 뽑은 후 도전과제 리스트 출력
+		// 회원이 아닐 때
+		if (user==null)
+		{
+			return "redirect:loginpage.do";
+		}
+		
+		// user통해 joinCode 얻기
+		// + joinCode로 groupApplyCode 얻기 + nickname도 얻어야함
+		String joinCode = "38";
+		groupApplyCode = "16";
+		
+		try
+		{
+			// 찾은 groupApplyCode로 리스트 출력
+			ArrayList<ChallengeDTO> challengeList = dao.challengeList(groupApplyCode);
+			
+			// 가져온 속성
+			// → challengeCode, joinCode, challengeType, title, startDate, content, createdDate
+			// 필요한 속성
+			// → endDate, status + nickname
+			
+			// 종료 날짜 구하기
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			for (ChallengeDTO dto : challengeList)
+			{
+				LocalDate start = LocalDate.parse(dto.getStartDate(), formatter);
+				LocalDate end=null;
+				
+				if ("1".equals(dto.getChallengeType()))			// 주간 과제
+					end = start.plusDays(7);
+				else if ("2".equals(dto.getChallengeType()))	// 월간 과제
+					end = start.plusDays(35);
+				
+				dto.setEndDate(end.format(formatter));
+				dto.setStatus("1");
+			}
+			
+			model.addAttribute("challengeList", challengeList);
+			
+		} catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		
+		return "/WEB-INF/view/group_room/challenge/ChallengeList.jsp";
+	}
+	
+	
+	// 도전과제 등록 페이지 출력
+	@RequestMapping(value="/challengecreatepage.do")
+	public String createChallengePage()
+	{
+		return "/WEB-INF/view/group_room/challenge/ChallengeCreate.jsp";
+	}
+	
+	
+	// 도전과제 등록 프로세스
+	@RequestMapping(value="/challengecreate.do", method=RequestMethod.POST)
+	public String createChallenge(HttpSession session, HttpServletRequest request
+			, ChallengeDTO dto)
+	{
+		/*
+		System.out.println(dto.getTitle());
+		System.out.println(dto.getStartDate());
+		System.out.println(dto.getContent());
+		System.out.println(dto.getChallengeType());
+		if (dto.getChallengeType().equals("daily"))
+		{
+			System.out.println(request.getParameter("day1"));
+			System.out.println(request.getParameter("day2"));
+			System.out.println(request.getParameter("day3"));
+			System.out.println(request.getParameter("day4"));
+			System.out.println(request.getParameter("day5"));
+			System.out.println(request.getParameter("day6"));
+			System.out.println(request.getParameter("day7"));
+		}
+		else
+		{
+			System.out.println("week1");
+			System.out.println("week2");
+			System.out.println("week3");
+			System.out.println("week4");
+			System.out.println("week5");
+		}
+		// challengeType (1 → daily, 2 → weekly)
+		*/
+		
+		IChallengeDAO dao = sqlSession.getMapper(IChallengeDAO.class);
+		
+		// 기존 dto에 있는 정보
+		// → title, startDate, Content, ChallengeType
+		// joinCode DTO에 집어넣기
+		UserDTO user = (UserDTO)session.getAttribute("user");
+		//String joinCode = dao.getJoinCode(user.getUserCode());
+		//dto.setJoinCode(joinCode);
+		
+		// 도전과제 세부 내용 입력용 DTO
+		ChallengeContentDTO contentDTO = new ChallengeContentDTO();
+		String type = request.getParameter(dto.getChallengeType());
+		
+		if ("".equals(type) || type==null)
+		{
+			throw new NullPointerException();
+		}
+		
+		if (type.equals("daily"))
+		{
+			contentDTO.setDay1(request.getParameter("day1"));
+			contentDTO.setDay2(request.getParameter("day2"));
+			contentDTO.setDay3(request.getParameter("day3"));
+			contentDTO.setDay4(request.getParameter("day4"));
+			contentDTO.setDay5(request.getParameter("day5"));
+			contentDTO.setDay6(request.getParameter("day6"));
+			contentDTO.setDay7(request.getParameter("day7"));
+		}
+		else
+		{
+			contentDTO.setWeek1(request.getParameter("week1"));
+			contentDTO.setWeek2(request.getParameter("week2"));
+			contentDTO.setWeek3(request.getParameter("week3"));
+			contentDTO.setWeek4(request.getParameter("week4"));
+			contentDTO.setWeek5(request.getParameter("week5"));
+		}
+		
+		
+		
+		return "/WEB-INF/view/group_room/challenge/ChallengeList.jsp";
+	}
+	
+	
+	// 도전과제 상세 내용 출력
+	@RequestMapping(value="/challengedetailpage.do")
+	public String challengeDetailPage(HttpSession session, HttpServletRequest request
+			, Model model)
+	{
+		IChallengeDAO dao = sqlSession.getMapper(IChallengeDAO.class);
+		// 이전 페이지에서 받는 정보
+		// 세션에서 유저, challengeCode
+		//UserDTO user = (UserDTO)session.getAttribute("user");
+		String challengeCode = request.getParameter("challengeCode");
+		if ("".equals(challengeCode) || challengeCode==null)
+			challengeCode = "2";		// 테스트용
+		
+		// 도전과제 출력
+		ChallengeDTO challengeDetail = dao.getChallengeDetail(challengeCode);
+		
+		// 도전과제 내용 출력
+		ArrayList<ChallengeContentDTO> challengeContentList = dao.getChallengeContent(challengeCode);
+		
+		model.addAttribute("challengeDetail", challengeDetail);
+		model.addAttribute("challengeContentList", challengeContentList);
+		
+		return "/WEB-INF/view/group_room/challenge/ChallengeDetail.jsp";
+	}
+	
+}
---
+++ FinalProject/250903_finalproject/src/com/test/mybatis/dto/ChallengeContentDTO.java
@@ -0,0 +1,125 @@
+package com.test.mybatis.dto;
+
+public class ChallengeContentDTO
+{
+	// 도전과제 등록 시 내용을 담을 속성
+	private String day1, day2, day3, day4, day5, day6, day7;
+	private String week1, week2, week3, week4, week5;
+	
+	// 도전과제 호출 시 담을 속성
+	private String round, content;
+	
+	public String getDay1()
+	{
+		return day1;
+	}
+	public void setDay1(String day1)
+	{
+		this.day1 = day1;
+	}
+	public String getDay2()
+	{
+		return day2;
+	}
+	public void setDay2(String day2)
+	{
+		this.day2 = day2;
+	}
+	public String getDay3()
+	{
+		return day3;
+	}
+	public void setDay3(String day3)
+	{
+		this.day3 = day3;
+	}
+	public String getDay4()
+	{
+		return day4;
+	}
+	public void setDay4(String day4)
+	{
+		this.day4 = day4;
+	}
+	public String getDay5()
+	{
+		return day5;
+	}
+	public void setDay5(String day5)
+	{
+		this.day5 = day5;
+	}
+	public String getDay6()
+	{
+		return day6;
+	}
+	public void setDay6(String day6)
+	{
+		this.day6 = day6;
+	}
+	public String getDay7()
+	{
+		return day7;
+	}
+	public void setDay7(String day7)
+	{
+		this.day7 = day7;
+	}
+	public String getWeek1()
+	{
+		return week1;
+	}
+	public void setWeek1(String week1)
+	{
+		this.week1 = week1;
+	}
+	public String getWeek2()
+	{
+		return week2;
+	}
+	public void setWeek2(String week2)
+	{
+		this.week2 = week2;
+	}
+	public String getWeek3()
+	{
+		return week3;
+	}
+	public void setWeek3(String week3)
+	{
+		this.week3 = week3;
+	}
+	public String getWeek4()
+	{
+		return week4;
+	}
+	public void setWeek4(String week4)
+	{
+		this.week4 = week4;
+	}
+	public String getWeek5()
+	{
+		return week5;
+	}
+	public void setWeek5(String week5)
+	{
+		this.week5 = week5;
+	}
+	public String getRound()
+	{
+		return round;
+	}
+	public void setRound(String round)
+	{
+		this.round = round;
+	}
+	public String getContent()
+	{
+		return content;
+	}
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	
+}
